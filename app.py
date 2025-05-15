87d79f08c05ae893977a04996179669a137aea99
okanaltin1993 <thinkpad123456789@gmx.de>
2025-05-15 14:20:41 +0200

    1

--- app.py
+++ app.py
@@ -71,6 +71,19 @@ def add_member():
 
     return redirect("/admin-panel")
 
+@app.route("/member-search")
+def member_search():
+    if not session.get("admin"):
+        return redirect("/admin-login")
+
+    conn = sqlite3.connect("club.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT mitgliedsnummer, vorname, nachname, strasse, mitgliedsstatus, plz, ort FROM mitglieder")
+    mitglieder = cursor.fetchall()
+    conn.close()
+
+    return render_template("member_search.html", mitglieder=mitglieder)
+
 @app.route("/delete-member", methods=["POST"])
 def delete_member():
     if not session.get("admin"):
@@ -85,7 +98,6 @@ def delete_member():
     conn.close()
 
     return redirect("/member-search")
-
 
 @app.route("/mitglied/<mitgliedsnummer>")
 def mitglied_detail(mitgliedsnummer):
